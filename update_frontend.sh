0551f80b665a2ebb9aaf47d5ff0d1ecf0174948b
Davide Gessa (dakk) <gessadavide@gmail.com>
2021-08-04 10:49:34 +0200

    fix subpath deploy

--- update_frontend.sh
+++ update_frontend.sh
@@ -1,5 +1,6 @@
 cp poollogs.json config.json pool-frontend/src/assets/
 cd pool-frontend
-ng build
+rm -r ../docs/*
+ng build --base-href=/lisk-pool3/
 cd ..
 cp -r pool-frontend/dist/pool-frontend/* docs
