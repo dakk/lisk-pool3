85d6d1b8ed92a6e95acae594e1bbc6730b211689
lemii <lemiidelegate@gmail.com>
2021-08-22 11:14:42 +0200

    add mainnet network identifier

--- liskpool3.py
+++ liskpool3.py
@@ -37,7 +37,7 @@ DRY_RUN = False
 
 NETWORKS = {
 	'testnet': '15f0dacc1060e91818224a94286b13aa04279c640bd5d6f193182031d133df7c',
-	'mainnet': 'notyetknow'
+	'mainnet': '4c09e6a781fc4c7bdb936ee815de8f94190f8a7519becd9de2081832be309a99'
 }
 
 
